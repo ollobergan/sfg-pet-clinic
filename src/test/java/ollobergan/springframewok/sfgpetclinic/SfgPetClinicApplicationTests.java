877e2ffaa5f428b42dfd169476243fec3945d10d
Ollobergan <karimov.ollobergan@gmail.com>
2020-05-12 05:49:36 +0500

    Initial commit

---
+++ src/test/java/ollobergan/springframewok/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ollobergan.springframewok.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
